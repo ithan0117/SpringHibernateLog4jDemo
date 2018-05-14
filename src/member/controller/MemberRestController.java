8ed4981b99c98407497d18da197dcc9d3d4c9f60
William.Lee <ithan0117@gmail.com>
2018-05-15 00:39:52 +0800

    memberRest - getImageExample, add @CrossOrigin

--- src/member/controller/MemberRestController.java
+++ src/member/controller/MemberRestController.java
@@ -13,6 +13,7 @@ import member.model.pojo.MemberBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,7 @@ public class MemberRestController {
 		return memberDAO.selectAllMembers();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:63342")//CORS support
 	@RequestMapping(value = "restfulGetImageExample", produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] getImageExample(HttpServletRequest request) {
 		try {
